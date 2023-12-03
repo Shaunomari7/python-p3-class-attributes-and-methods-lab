94c34f179c2eef9ad166b5ee29b8103d9631c7c7
Shaun Omari <Shaun@Sandras-MacBook-Air-2.local>
2023-12-03 14:18:56 +0300

    submit

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,50 @@
+#!/usr/bin/env python3
+
 class Song:
-    pass
+
+    count = 0
+    genres = []
+    artists = []
+    genre_count = []
+    artist-count = []
+    
+    def __init__(self, title, artist, genre):
+        self.title = title
+        self.artist = artist
+        self.genre = genre
+        self.add_to_song_count()
+        self.add_to_genres(genre)
+        self.add_to_artists(artist)
+        self.add_to_genre_count(genre)
+        self.add_to_artist_count(artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genres):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artists):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if cls.genre_count.get(genre):
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if cls.artist_count.get(artist):
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
+
+
+   
